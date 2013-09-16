7611a3184cd63dc73d44240e55c822c971a3d060
idsc3101 <idsc3101@gmail.com>
2013-09-16 17:19:11 -0500

    Solved the problem

--- FirstName/FirstName/Form1.vb
+++ FirstName/FirstName/Form1.vb
@@ -3,6 +3,23 @@
 Public Class Form1
 
     Private Sub btnFirstName_Click(sender As System.Object, e As System.EventArgs) Handles btnFirstName.Click
+        Dim fullName As String
+        'Read the input from the user in the full name text box
+        fullName = txtFullName.Text
 
+        Dim fullNameTrimmed As String
+        fullNameTrimmed = fullName.Trim()
+        'trimmed full name
+
+        Dim idxSpace As Integer
+        idxSpace = fullNameTrimmed.IndexOf(" ")
+        'Use indexof to find the space
+
+        Dim firstname As String
+        firstname = fullNameTrimmed.Substring(0, idxSpace)
+        'pull first name from full name
+
+        txtFirstName.Text = firstname
+        'Display it back to the user
     End Sub
 End Class
